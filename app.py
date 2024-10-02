8827b2e03576478c2eedfa48dceb115a9c50d226
Nick <qdqd@acvvrv.com>
2024-10-02 21:30:43 +0300

    new feature: you car run "app.py --debug" to catch and log any message to debug

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 # app.py
+import asyncio
+import argparse
 
 from apps.t.client import TelegramClient
 from apps.x.client import XClient
-from apps.t.handlers import create_photo_message_handler    #*, create_debug_message_handler
+from apps.t.handlers import create_photo_message_handler
 from settings.config import Config
-import asyncio
 
+def parse_arguments() -> argparse.Namespace:
+    """Parse command-line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="Run the Telegram bot")
+    parser.add_argument(
+        "--debug", action="store_true", help="Activate debug mode", default=False
+    )
+    return parser.parse_args()
 
 async def main():
     config = Config()
@@ -14,11 +26,17 @@ async def main():
 
     # Create message handler, passing x_client
     message_handler = create_photo_message_handler(telegram_client, x_client)
-    #* debug_handler = create_debug_message_handler()
 
+    # Parse command-line arguments
+    args: argparse.Namespace = parse_arguments()
+    
+    if args.debug:
+        from apps.t.handlers import create_debug_message_handler
+        debug_handler = create_debug_message_handler()
+        telegram_client.add_handler(debug_handler)
+    
     # Add handler to client
     telegram_client.add_handler(message_handler)
-    #* telegram_client.add_handler(debug_handler)
 
     # Run client
     await telegram_client.run()
